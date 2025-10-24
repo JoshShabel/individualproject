6c0d7da40204605be82869c7c9f8fd64aa809c98
JoshShabel <joshshabel@gmail.com>
2025-10-24 08:57:43 -0400

    Toggle burger when clicking link

--- src/subpage/Sidebar.jsx
+++ src/subpage/Sidebar.jsx
@@ -8,14 +8,14 @@ import projectData from './projectData.json';
 import LowWidthContext from "../LowWidthContext.jsx";
 import {useContext, useState} from "react";
 
-const SidebarList = ({lowWidth}) => {
+const SidebarList = ({lowWidth, setButtonOne}) => {
     return (
         <div>
             <h4>Learn about OpenWrt</h4>
             <div className={lowWidth ? styles.sidebarStyleLowWidth : styles.sidebarStyleHighWidth}>
                 <ul>
                     {learnData.list.map(item => (
-                        <li key={item}><a key={item} href={'#/' + "learn" + '/' + item.link}>{item.text}</a>
+                        <li key={item}><a key={item} href={'#/' + "learn" + '/' + item.link} onClick={() => setButtonOne(false)}>{item.text}</a>
                         </li>))}
                 </ul>
             </div>
@@ -23,7 +23,7 @@ const SidebarList = ({lowWidth}) => {
             <div className={lowWidth ? styles.sidebarStyleLowWidth : styles.sidebarStyleHighWidth}>
                 <ul>
                     {contributingData.list.map(item => (
-                        <li key={item}><a key={item} href={'#/' + "contributing" + '/' + item.link}>{item.text}</a>
+                        <li key={item}><a key={item} href={'#/' + "contributing" + '/' + item.link} onClick={() => setButtonOne(false)}>{item.text}</a>
                         </li>))}
                 </ul>
             </div>
@@ -31,7 +31,7 @@ const SidebarList = ({lowWidth}) => {
             <div className={lowWidth ? styles.sidebarStyleLowWidth : styles.sidebarStyleHighWidth}>
                 <ul>
                     {projectData.list.map(item => (
-                        <li key={item}><a key={item} href={'#/' + "project" + '/' + item.link}>{item.text}</a>
+                        <li key={item}><a key={item} href={'#/' + "project" + '/' + item.link} onClick={() => setButtonOne(false)}>{item.text}</a>
                         </li>))}
                 </ul>
             </div>
@@ -52,19 +52,20 @@ function Sidebar() {
                     Learn about OpenWrt
                 </button>
                 {buttonOne && (
-                <SidebarList lowWidth={lowWidth}/>
+                <SidebarList lowWidth={lowWidth} setButtonOne={setButtonOne} />
                 )}
             </div>
         );
     } else {
         return (
-            <SidebarList lowWidth={lowWidth}/>
+            <SidebarList lowWidth={lowWidth} setButtonOne={setButtonOne} />
         );
     }
 }
 
 SidebarList.propTypes = {
     lowWidth: PropTypes.any,
+    setButtonOne: PropTypes.any,
 }
 
 Sidebar.propTypes = {
